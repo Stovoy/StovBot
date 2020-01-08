ba99a20d52080412ffe003bbd206346e7ad8c507
Steve Mostovoy <stevemostovoysm@gmail.com>
2020-01-08 07:46:59 -0800

    Mount a /sql

--- scripts/usql.sh
+++ scripts/usql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 set -o pipefail
-docker run -it -v $(pwd)/db.db3:/db.db3 -v /root/.usql_history:/root/.usql_history stovoy/usql /db.db3
+docker run -it -v $(pwd)/db.db3:/db.db3 -v /root/.usql_history:/root/.usql_history -v $(pwd)/sql:/sql stovoy/usql /db.db3
